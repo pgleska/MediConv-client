0680871ea727e4933e5492ce6b1ca14ad97895ce
impogran <gerardbosacki@gmail.com>
2022-11-12 20:23:33 +0100

    update szaty graficznej, register, profile, menu profilu nie dziala

--- app/src/main/java/com/github/pgleska/ui/register/RegisterFragment.java
+++ app/src/main/java/com/github/pgleska/ui/register/RegisterFragment.java
@@ -1,4 +1,4 @@
-package com.github.pgleska.ui.login;
+package com.github.pgleska.ui.register;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,19 +11,18 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.github.pgleska.R;
-import com.github.pgleska.databinding.FragmentLoginBinding;
+import com.github.pgleska.databinding.FragmentRegisterBinding;
 
-public class LoginFragment extends Fragment {
+public class RegisterFragment extends Fragment {
 
-    private FragmentLoginBinding binding;
+    private FragmentRegisterBinding binding;
     private View root;
     private Button ButtonRegister;
-    private Button ButtonLogin;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        binding = FragmentRegisterBinding.inflate(inflater, container, false);
         root = binding.getRoot();
 
         initComponents();
@@ -39,16 +38,13 @@ public class LoginFragment extends Fragment {
     }
 
     private void initComponents() {
-        ButtonRegister = binding.btnToRegister;
-        ButtonLogin = binding.btnLogin;
+        ButtonRegister = binding.btnRegister;
     }
 
     private void initListeners() {
         ButtonRegister.setOnClickListener(v -> {
-            Navigation.findNavController(root).navigate(R.id.action_nav_login_to_nav_register);
-        });
-        ButtonLogin.setOnClickListener(v -> {
-            Navigation.findNavController(root).navigate(R.id.action_nav_login_to_nav_conversations);
+            Navigation.findNavController(root).navigate(R.id.action_nav_register_to_nav_conversations);
         });
     }
 }
+
